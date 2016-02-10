c794c64bf34261e3725726d332ce0845617aa91a
Fred J. Hickernell <hickernell@iit.edu>
2016-02-10 13:51:42 -0600

    Row Operations

---
+++ RowOperationsExample.m
@@ -0,0 +1,21 @@
+%% Row Operations Example
+% This script works out some of the examples in the notes
+
+%% Solving \(\mathsf{A}\boldsymbol{x} = \boldsymbol{b}) Using Row Operations
+
+InitializeWorkspaceDisplay %initialize the workspace and the display parameters
+A = [3 -1 4
+   0 2 1
+   -6 5 2] %our original matrix
+b = [13
+   7
+   10] %our original right hand vector
+
+%%
+% First
+
+   
+%% Now Using Partial Pivoting
+
+%%
+% _Author: Fred J. Hickernell_
