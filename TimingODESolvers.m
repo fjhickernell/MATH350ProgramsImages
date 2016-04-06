bf0b7210de60b4e3b4825b4a30264529acdccf18
Fred J. Hickernell <hickernell@iit.edu>
2016-04-06 15:45:08 -0500

    A few more changes

--- TimingODESolvers.m
+++ TimingODESolvers.m
@@ -30,7 +30,7 @@ alpha = 1.5; %low population growth rate
 beta = 1000; %limiting population
 f = @(t,y) alpha*y.*(1 - y/beta); %right-hand-side of the ODE
 y0 = 10; %initial condition
-tint = [0,4]; %time interval
+tint = [0,6]; %time interval
 tcheck = (tint(1):0.0002:tint(2)); %times to check against the exact solution
 yexact = @(t) y0*beta./(y0 + (beta-y0)*exp(-alpha*t)); %exact solution
 ycheck = yexact(tcheck); %places to check the exact solution with the approximations
@@ -38,6 +38,10 @@ MATLABColors = [0, 0.447, 0.741; ... %these are
    0.85,  0.325, 0.098; ... %MATLAB plotting
    0.494,  0.184, 0.556; ... %colors that 
    0.466,  0.674, 0.188]; %we want to use
+figure
+plot(tcheck,ycheck,'-')
+xlabel('Time')
+ylabel('Population')
 
 %% Euler's method
 % We will compute the Euler's method approximation to the population ODE
@@ -46,7 +50,7 @@ MATLABColors = [0, 0.447, 0.741; ... %these are
 hvec = [0.5 0.2 0.1 0.05 0.02 0.01 0.005 0.002 0.001]'; %vector of time steps
 nvec = (tint(2)-tint(1))./hvec; %numbers of time steps
 nh = numel(hvec); %number of different time steps
-errEuler = zeros(nh,1); %initialize graphics handle
+errEuler = zeros(nh,1); %initialize error vector
 for i=1:nh;
    [tEuler,yEuler] = Euler(f,tint,y0,hvec(i)); %compute by Euler's method
    yapprox = spline(tEuler,yEuler,tcheck); %values of y to check
@@ -57,7 +61,7 @@ h = loglog(nvec,errEuler,'.','color',MATLABColors(1,:));
 legtext = {'Euler Method'};
 legend(h,legtext,'location','northeast')
 legend boxoff
-xlabel('\(h\)')
+xlabel('\(n\)')
 ylabel('Error')
 hold on
 
@@ -79,6 +83,19 @@ loglog(nvec([1 nh]),cEuler * nvec([1 nh]).^-pEuler, ...
 
 %% Modified Euler's method
 % Let's do the same for the Modified Euler's Method.
-
-
+errMEuler = zeros(nh,1); %initialize error vector
+for i=1:nh;
+   [tMEuler,yMEuler] = modifiedEuler(f,tint,y0,hvec(i)); %compute by the modified Euler method
+   yapprox = spline(tMEuler,yMEuler,tcheck); %values of y to check
+   errMEuler(i) = max(abs(ycheck - yapprox));
+end
+coeff = [ones(nh,1) log(nvec)] \ log(errMEuler); %linear regression with \
+pMEuler = -coeff(2) %the p value
+cMEuler = exp(coeff(1)) %the c value
+h = [h; loglog(nvec,errMEuler,'.', ...
+   nvec([1 nh]),cMEuler * nvec([1 nh]).^-pMEuler,'-', ...
+   'color',MATLABColors(2,:))];
+legtext{2} = 'Modified Euler Method';
+legend(h,legtext,'location','northeast')
+legend boxoff
 toc
